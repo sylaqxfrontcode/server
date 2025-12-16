9592cf8f5e6fa2270339531bd34f6a9bb259a332
Shubhadeep Nandi <shubhadeepnandi03@gmail.com>
2025-12-16 09:57:18 +0530

    push thr changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // If you are using /api in frontend URLs
+  app.setGlobalPrefix('api');
+
+  // Enable CORS for frontend
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
